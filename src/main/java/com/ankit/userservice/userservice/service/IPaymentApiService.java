772bb340ff4180352ba54a3f0850f0fa5baffd14
ankit.kumar2 <ankit.kumar@payufin.com>
2022-10-08 12:19:23 +0530

    USRVC-2:  - removed unnecessary imports.  - revered the changes.  - fixed the build issue.

--- src/main/java/com/ankit/userservice/userservice/service/IPaymentApiService.java
+++ src/main/java/com/ankit/userservice/userservice/service/IPaymentApiService.java
@@ -1,17 +1,11 @@
 package com.ankit.userservice.userservice.service;
 
 import com.ankit.userservice.userservice.dto.PaymentInfoDTO;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 public interface IPaymentApiService {
-    @GetMapping("/paymentInfo/{userId}")
-    public PaymentInfoDTO getPaymentInfoForUser(@PathVariable("userId") String userId);
+    PaymentInfoDTO getPaymentInfoForUser(String userId);
 
-    @GetMapping("/allPaymentInfo")
-    public List<PaymentInfoDTO> getAllPaymentInfo();
+    List<PaymentInfoDTO> getAllPaymentInfo();
 }
